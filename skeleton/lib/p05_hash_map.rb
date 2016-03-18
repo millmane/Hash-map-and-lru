a3879d9f6ff389e4e6f9a4a1427b84917acc4cc9
App Academy Student <student@appacademy.io>
2016-03-18 15:49:10 -0700

    Implemented custom HashMap

--- skeleton/lib/p05_hash_map.rb
+++ skeleton/lib/p05_hash_map.rb
@@ -2,26 +2,50 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_reader :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
     @count = 0
   end
+  #
+  # def inspect
+  # end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    resize! if count == num_buckets
+
+    if include?(key)
+      bucket(key).each do |link|
+        if link.key == key
+          link.val = val
+          break
+        end
+      end
+    else
+      bucket(key).insert(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
   def each
+    @store.each do |linked_list|
+      linked_list.each { |link| yield(link.key, link.val) }
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -42,9 +66,18 @@ class HashMap
   end
 
   def resize!
+    pairs = []
+
+    each { |key, val| pairs << [key, val] }
+
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 0
+
+    pairs.each { |pair| set(*pair) }
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
