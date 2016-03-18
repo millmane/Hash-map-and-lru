43d6377e4218f6737de9e36c524d55f57a38cd0f
App Academy Student <student@appacademy.io>
2016-03-18 11:12:18 -0700

    created IntSet

---
+++ intset.rb
@@ -0,0 +1,17 @@
+class IntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(int)
+    @store[int] = true
+  end
+
+  def remove(int)
+    @store[int] = false
+  end
+
+  def include?(int)
+    @store[int]
+  end
+end
