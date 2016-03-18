da8ce9aaefca693117fd178765c3dfec26689120
App Academy Student <student@appacademy.io>
2016-03-18 11:35:52 -0700

    moved work into skeleton directory supplied by a/A

---
+++ skeleton/lib/p01_int_set.rb
@@ -0,0 +1,93 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+    @max = max
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    validate!(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num < @max && num >= 0
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
+  end
+
+  def insert(num)
+    @store[num % @num_buckets] << num
+  end
+
+  def remove(num)
+    @store[num % @num_buckets].delete(num)
+  end
+
+  def include?(num)
+    @store[num % @num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % @num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
